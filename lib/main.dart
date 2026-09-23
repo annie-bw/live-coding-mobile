0b0f5b005f51e6c03b9ca4660fdf7d9199e10f3b
RobertNiyonkuru98 <r.niyonkuru@alustudent.com>
2026-09-23 12:52:17 +0200

    feat: finalized the initial main.dart setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: LiveDemoForm();
+    home: LiveDemoForm(),
   ));
 }
 
@@ -16,7 +16,33 @@ class LiveDemoForm extends StatefulWidget {
 class _LiveDemoFormState extends State<LiveDemoForm> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Room 8"),
+          centerTitle:true,
+          backgroundColor: Colors.red,
+        ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                color: Colors.white,
+                padding:EdgeInsets.symmetric(vertical: 30),
+                child: Center(
+                  child: Text('Room 8',
+                      style: TextStyle(
+                          fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      )),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
 
